5d902460aa8b25815ccd09c194821b49fe19dd67
Artem <alexx1088@gmail.com>
2022-09-13 12:56:03 +0600

    add users to db

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,8 @@ class UserFactory extends Factory
             'name' => $this->faker->firstName(),
             'surname' => $this->faker->lastName(),
             'patronymic' => $this->faker->firstNameMale(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'email' => $this->faker->unique()->email,
+            //     'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2
             .uheWG/igi', // password
             'data_of_birth' => $this->faker->dateTimeBetween('1970-01-01', '2003-12-31')
